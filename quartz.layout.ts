fd376d2738b39b44210d37af0132981b689c51d5
jobo <jo-bo@qq.com>
2026-04-26 14:02:52 +0800

    style: remove explorer navigation

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -37,7 +37,6 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
   ],
   right: [
     Component.Graph(),
@@ -61,7 +60,6 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
   ],
   right: [],
 }
